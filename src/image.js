b2140f1639b274e34211b96c3695574917329a54
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-08-08 17:25:34 +0100

    Update js

--- src/image.js
+++ src/image.js
@@ -1,7 +1,17 @@
-const fileInput = document.querySelector(".fileinputimg");
-const chooseimgbtn = document.querySelector(".chooseimg");
-const previewImg = document.querySelector(".previewimg img");
+const fileInput = document.querySelector(".fileinputimg"),
+ chooseimgbtn = document.querySelector(".chooseimg"),
+ previewImg = document.querySelector(".previewimg img"),
+ filterOptions = document.querySelectorAll(".fcol btn button"),
+filterName = document.querySelector(".filter-info .name"),
+ filterValue = document.querySelector(".filter-info .value"),
+ filterSlider = document.querySelector(".slider input");
 
+let imageName,
+  disableEditor = true;
+let brightness = "100",
+  saturation = "100",
+  inversion = "0",
+  grayscale = "0";
 
 //////image loading
 const loadImage = () => {
@@ -11,10 +21,29 @@ const loadImage = () => {
     previewImg.addEventListener("load", () => {
         document.querySelector (".container").classList.remove("disable")
     });
-    console.log(file);
+    //console.log(file);
 }
+////////filter 
 
-fileInput, addEventListener("change", loadImage);
+filterOptions.forEach(option => {
+  console.log(option);
+  option.addEventListener("click", () => {
+    if (disableEditor) return;
+    document.querySelector(".fcol .active").classList.remove("active");
+    option.classList.add(" active");
+    filterName.innerText = option.innerText;
+
+
+  });
+});
+const updateFilter = () => {
+//console.log(filterSlider.value);
+   filterValue.innerText = `${filterSlider.value}%`;
+};
+
+
+fileInput.addEventListener("change", loadImage);
+filterSlider.addEventListener("input", updateFilter);
 /// choose image event click
 chooseimgbtn.addEventListener("click",() => fileInput.click());
 
