6f5abddf1798605d99cb547f12acdf99027bf98b
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-08-08 16:00:56 +0100

    update js

--- src/image.js
+++ src/image.js
@@ -1,8 +1,16 @@
 const fileInput = document.querySelector(".fileinputimg");
 const chooseimgbtn = document.querySelector(".chooseimg");
+const previewImg = document.querySelector(".previewimg img");
 
+
+//////image loading
 const loadImage = () => {
     let file =fileInput.files[0];
+    if(!file) return;
+    previewImg.src =URL.createObjectURL(file); ////passing file though url as preview img src
+    previewImg.addEventListener("load", () => {
+        document.querySelector (".container").classList.remove("disable")
+    });
     console.log(file);
 }
 
