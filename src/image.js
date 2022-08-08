44319b9ede242829104a906c134f711e57795a95
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-08-08 15:14:15 +0100

    Update image js

---
+++ src/image.js
@@ -0,0 +1,6 @@
+const fileInput = document.querySelector(".file-inputimg");
+const chooseimgbtn = document.querySelector(".chooseimg");
+
+
+chooseimgbtn.addEventListener("click",() => fileInput.click());
+
