9c625c4a92c2b90f1bbaddfdc9a98601be44bd3e
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-08-08 15:42:33 +0100

    update js

--- src/image.js
+++ src/image.js
@@ -1,6 +1,12 @@
-const fileInput = document.querySelector(".file-inputimg");
+const fileInput = document.querySelector(".fileinputimg");
 const chooseimgbtn = document.querySelector(".chooseimg");
 
+const loadImage = () => {
+    let file =fileInput.files[0];
+    console.log(file);
+}
 
+fileInput, addEventListener("change", loadImage);
+/// choose image event click
 chooseimgbtn.addEventListener("click",() => fileInput.click());
 
